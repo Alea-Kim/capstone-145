497cba8f126e2f68563ec3914ceabd90a9fcc5dd
RoseyG <mrosariotg@gmail.com>
2017-05-02 01:06:59 +0800

    Initial commit - added forders for sketches

---
+++ TEST_ULTRASONIC_6SENSORS/TEST_ULTRASONIC_6SENSORS.ino
@@ -0,0 +1,46 @@
+/*
+* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+*
+* Crated by Dejan Nedelkovski,
+* www.HowToMechatronics.com
+*
+*/
+// defines pins numbers
+int trigPin = 1;
+int echoPin = 2;
+// defines variables
+long duration;
+int distance;
+void setup() {
+for(int i = 0; i<6;i++){
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  trigPin+=2;
+  echoPin+=2;
+}
+Serial.begin(9600); // Starts the serial communication
+Serial.print("TEST");
+}
+void loop() {
+if (trigPin == 13) trigPin = 1;
+if (echoPin == 14) echoPin = 2;
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Sensor: ");
+Serial.println(echoPin/2);
+Serial.print("Distance: ");
+Serial.println(distance);
+delay(100);
+trigPin+=2;
+echoPin+=2;
+}
