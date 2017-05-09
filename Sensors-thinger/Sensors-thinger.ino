ccb2b532b6825aba284213698d804af20e6cf8d3
Christelle Divina <christelleanne16@gmail.com>
2017-05-10 02:12:06 +0800

    thinger update

--- Sensors-thinger/Sensors-thinger.ino
+++ Sensors-thinger/Sensors-thinger.ino
@@ -10,7 +10,7 @@ int echoPin[5] = {A4, A2, 3, 5, 7};
 
 // defines variables
 long duration;
-int distance;
+int distance, ch=0;
 
 void setup() {
   uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
@@ -20,88 +20,30 @@ void setup() {
     pinMode(echoPin[i], INPUT); // Sets the echoPin as an Input
   }
 
-  thing["sensor1"] >> [] (pson & out) {
-    // Clears the trigPin
-    digitalWrite(trigPin[0], LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 50 micro seconds
-    digitalWrite(trigPin[0], HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin[0], LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin[0], HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    out = distance;
-    delay(100);
-  };
-  thing["sensor2"] >> [] (pson & out) {
-    // Clears the trigPin
-    digitalWrite(trigPin[1], LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 50 micro seconds
-    digitalWrite(trigPin[1], HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin[1], LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin[1], HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    out = distance;
-    delay(100);
-  };
-  thing["sensor3"] >> [] (pson & out) {
-    // Clears the trigPin
-    digitalWrite(trigPin[2], LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 50 micro seconds
-    digitalWrite(trigPin[2], HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin[2], LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin[2], HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    out = distance;
-    delay(100);
-  };
-  thing["sensor4"] >> [] (pson & out) {
-    // Clears the trigPin
-    digitalWrite(trigPin[3], LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 50 micro seconds
-    digitalWrite(trigPin[3], HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin[3], LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin[3], HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    out = distance;
-    delay(100);
-  };
-  thing["sensor5"] >> [] (pson & out) {
-    // Clears the trigPin
-    digitalWrite(trigPin[4], LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 50 micro seconds
-    digitalWrite(trigPin[4], HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin[4], LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin[4], HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    out = distance;
-    delay(100);
+  thing["sensors"] >> [] (pson & out) {
+    out["sensor1"] = getDist(0);
+    out["sensor2"] = getDist(1);
+    out["sensor3"] = getDist(2);
+    out["sensor4"] = getDist(3);
+    out["sensor5"] = getDist(4);
+    delay(10);
   };
 }
 
 void loop() {
   thing.handle();
+}
+
+int getDist(int ch){
+  digitalWrite(trigPin[ch], LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 50 micro seconds
+  digitalWrite(trigPin[ch], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin[ch], LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin[ch], HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  return distance;
 }
